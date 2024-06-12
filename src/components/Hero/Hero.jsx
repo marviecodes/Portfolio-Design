922d5e7b5b04fa723a594efde7ae448768974d64
Marvelous Osondu <marvnonso554@gmail.com>
2024-06-12 22:02:29 +0100

    Finished Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,42 @@
 import "./Hero.css";
+import { profile1 } from "../../assets/images/";
+import Points from "./Points/Points";
+import Partners from "./Partners/Partners";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section id="home">
+      {/* <div className="stroke__text intro__text">HELLO</div> */}
+      <div className="container full__height">
+        <div className="info__container">
+          <div className="user__profile">
+            <div className="photo__container primary__effect">
+              <img src={profile1} alt="" />
+            </div>
+          </div>
+
+          <div className="details__container">
+            <h3>
+              Hello there! Im{" "}
+              <span className="color__primary">Marvelous Osondu</span>
+            </h3>
+
+            <h1 className="title">
+              <span className="text-gradient">Fullstack Developer</span>
+            </h1>
+            <h3>Working from Nigeria</h3>
+            <p className="text__muted description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+              dolore. Atque laudantium ipsum dicta incidunt.
+            </p>
+            <Points />
+          </div>
+        </div>
+
+        <Partners />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
---
+++ src/components/Hero/Partners/Partners.jsx
@@ -0,0 +1,16 @@
+import "./Partners.css";
+import { partners } from "../../../data";
+
+const Partners = () => {
+  return (
+    <div id="partners">
+      <div className="logos__slide">
+        {partners.map((partner, index) => (
+          <img src={partner.logo} alt={partner.name} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Partners;
